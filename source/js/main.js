7a73855b02de1a882b0d23482b4802f28917983f
Yaroslav Fedosenko <83610007+aysaY-OvO@users.noreply.github.com>
2023-12-12 01:00:42 +0300

    add about and subscripion blocks

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,9 @@
 import {iosVhFix} from './utils/ios-vh-fix.js';
-import {initModals} from './modules/modals/init-modals.js';
 import {Form} from './modules/form-validate/form.js';
+import {initTabs} from './modules/tabs/init-tabs.js';
 
 // ---------------------------------
+const playVideo = document.querySelector('[data-button="play-video"]');
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -17,7 +18,12 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+    playVideo.addEventListener('click', (evt) => {
+      evt.target.classList.toggle('no-pseudo');
+      evt.target.innerHTML = '<iframe width="364" height="228" src="https://www.youtube.com/embed/9TZXsZItgdw?si=5WMu_IILpRD6bLQr" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+    });
+
+    initTabs();
     const form = new Form();
     window.form = form;
     form.init();
