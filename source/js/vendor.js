94e7ec8d5c05409b9d989138c00d171486905c6e
Yaroslav Fedosenko <83610007+aysaY-OvO@users.noreply.github.com>
2023-12-18 11:05:23 +0300

    добавил свайпер в vendor.js

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,9 @@
 // Swiper 8.4.7
-// import './vendor/swiper.js';
+import {Navigation, Swiper} from './vendor/swiper.js';
 import './vendor/focus-visible-polyfill.js';
+
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation],
+});
+swiper();
